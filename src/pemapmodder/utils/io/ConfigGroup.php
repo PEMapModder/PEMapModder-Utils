a6245e061fa39d1d44d4c63c837577add1bb563a
PEMapModder <pemapmodder1970@gmail.com>
2014-04-06 18:42:37 +0800

    Some  framework workings

--- src/pemapmodder/utils/io/ConfigGroup.php
+++ src/pemapmodder/utils/io/ConfigGroup.php
@@ -4,5 +4,16 @@ namespace pemapmodder\utils\io;
 
 use pocketmine\utils\Config;
 
-class CfgGrp{
+class CfgGrp {# implements ArrayAccess{
+	public function __construct($dir, array $defaults){
+		@mkdir($dir);
+		$this->dir = $dir;
+		foreach($defaults as $file=>$data){
+			$this->create($file, $data);
+		}
+	}
+	protected function create($file, $data){
+		if(@$data["create-independent-dir"] === true){
+			
+		}
 }
