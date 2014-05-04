1a926da7d5044397b8b248167be892ce63cd0e01
PEMapModder <pemapmodder1970@gmail.com>
2014-05-04 19:30:17 +0800

    Something I'm sure somebody and someone will shout at me

--- src/pemapmodder/utils/PluginCmdExt.php
+++ src/pemapmodder/utils/PluginCmdExt.php
@@ -9,16 +9,18 @@ use pocketmine\command\PluginCommand as ParentClass;
 use pocketmine\plugin\Plugin;
 
 class PluginCmdExt extends ParentClass{
+	protected $pexecutor;
+	protected $doParent = false;
 	public function __construct($name, Plugin $plugin, CmdExe $exe = null){
 		parent::__construct($name, $plugin);
-		$this->executor = $exe === null ? $plugin:$exe;
+		$this->pexecutor = $exe === null ? $plugin:$exe;
 	}
 	public function execute(Issuer $issuer, $label, array $args){
-		if(!$this->owningPlugin->isEnabled())
+		if(!$this->getPlugin()->isEnabled())
 			return false;
 		if(!$this->testPermission($issuer))
 			return false;
-		$result = $this->executor->onCommand($issuer, $this, $label, $args);
+		$result = $this->pexecutor->onCommand($issuer, $this, $label, $args);
 		if(is_null($result))
 			return true;
 		if(is_bool($result)){
@@ -27,10 +29,21 @@ class PluginCmdExt extends ParentClass{
 			return $result;
 		}
 		if(!is_string($result)){
-			trigger_error("Unexpected return type from ".Font::YELLOW.get_class($this->executor)."::onCommand(CommandSender, Command, string, array)".Font::RED.", !(string|bool|null): ".print_r($result, true), E_USER_ERROR);
+			trigger_error("Unexpected return type from ".Font::YELLOW.get_class($this->executor)."::onCommand(CommandSender, Command, string, array)".Font::RED.", !(string|bool|null): ".print_r($result, true), E_USER_WARNING);
 			return true;
 		}
 		$issuer->sendMessage("$result");
 		return true;
+	}
+	public function register(CommandMap $map = null){ // I know somebody will shout at me on this.
+		if($map === null)
+			$map = Server::getInstance()->getCommandMap();
+		if(!$this->doParent){
+			$this->doParent = true;
+			$map->register($this->getPlugin()->getName(), $this);
+			$this->doParent = false;
+			return;
+		}
+		parent::register($map);
 	}
 }
